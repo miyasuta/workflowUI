2dfd0387e7caeabf50fc0389effbc0cc9bd2273f
Mio Yasutake <mio.fujita.01@gmail.com>
2021-08-06 11:30:48 +0900

    get processors by function

--- taskUI/webapp/controller/App.controller.js
+++ taskUI/webapp/controller/App.controller.js
@@ -16,6 +16,7 @@ sap.ui.define([
                 approvalSteps: [],
                 requestId: "",
                 subject: "",
+                requester: "",
                 input: {
                     enabled: true
                 }
@@ -104,18 +105,39 @@ sap.ui.define([
         },
 
         _getWorflowData: function (workflowId) {
+            //context bining for workflow data
             var oModel = this.getModel();
-            this._oContextBinding = oModel.bindContext(`/WorkflowInstances(${workflowId})`, null, {
-                $expand: "Processors"
+            this._oContextBinding = oModel.bindContext(`/WorkflowInstances(${workflowId})`);
+
+            return Promise.all([
+                this._oContextBinding.requestObject(),
+                this._getProcessors()
+            ]);
+        },
+
+        _getProcessors: function () {
+            return new Promise((resolve, reject)=>{
+                var oModel = this.getModel();
+                var oContext = oModel.createBindingContext(`/WorkflowInstances(${this._workflowId})`);
+                var oFunction = oModel.bindContext("WorkflowService.getProcessors(...)", oContext);
+                oFunction.execute()
+                .then(()=>{
+                    resolve(oFunction.getBoundContext().getObject());
+                })
+                .catch((err)=>{
+                    reject(err);
+                });
             });
-            return this._oContextBinding.requestObject();
         },
 
         _doBind: function (data) {
-            this.getModel("viewModel").setProperty("/requestId", data.businessKey);
-            this.getModel("viewModel").setProperty("/subject", data.subject);
+            var workflowData = data[0];
+            var processors = data[1].value;
+            this.getModel("viewModel").setProperty("/requestId", workflowData.businessKey);
+            this.getModel("viewModel").setProperty("/subject", workflowData.subject);
+            this.getModel("viewModel").setProperty("/requester", workflowData.requester);
             this.getModel("viewModel").setProperty("/input/enabled", true);
-            var processors = data.Processors.map(processor=>{
+            var processors = processors.map(processor=>{
                 return {
                     id: processor.userId,
                     comment: processor.comment,
@@ -137,7 +159,15 @@ sap.ui.define([
             var data = this._createRequestData();
             var url = this.getBaseURL() + `/workflow/WorkflowInstances(${this._workflowId})`;
             this.getView().setBusy(true);
-            this._sendRequest(url, data, decision)
+            var reworkObj = {};
+            if (decision === "reject" )
+            {
+                reworkObj = {
+                    userId: this.getModel("viewModel").getProperty("/requester"), //temp
+                    isRequester: true
+                }
+                        }
+            this._sendRequest(url, data, decision, reworkObj)
             .then(()=>{
                 this.getView().setBusy(false);
                 this._refreshTask();
@@ -170,13 +200,16 @@ sap.ui.define([
             return data;
         },
 
-        _sendRequest: function (url, data, decision) {
+        _sendRequest: function (url, data, decision, reworkObj) {
             return new Promise((resolve, reject)=>{
                 var xhr = new XMLHttpRequest();
                 xhr.open("PATCH", url);
                 xhr.setRequestHeader("content-type", "application/json");
                 xhr.setRequestHeader("decision", decision);
                 xhr.setRequestHeader("taskInstanceId", this._getTaskInstanceId());
+                if (decision === "reject" ){
+                    xhr.setRequestHeader("reworkProcessor", JSON.stringify(reworkObj));
+                }
                 xhr.onload = function () {
                     if (xhr.status === 200) {
                         resolve();
